763a93b75f9065548c618e7e73de4e1b0debea15
Shahriar Rumel <rumelshahriarhere@gmail.com>
2023-03-22 00:24:39 +0600

    chore: add react navigation stack and native package

--- App.js
+++ App.js
@@ -15,6 +15,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
